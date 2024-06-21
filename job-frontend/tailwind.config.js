cd65763d6b3dc314d5e537f2cbfcdbde8a77d3c7
sa11748 <shubhangishubhi490@gmail.com>
2024-06-21 08:56:37 +0530

    added 5 files

---
+++ job-frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/styles/tailwind.css',
+  ],
+  theme: {
+    extend: {
+      colors : {
+      "darkblue" : "#111827",
+      "cherry" : "#ff0056",
+      "white" : "fff",
+      "black" : "#000",
+      },
+    },
+  },
+  plugins: [],
+}
+
